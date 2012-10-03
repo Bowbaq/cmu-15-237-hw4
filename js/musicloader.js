f6ec3bb954c40425d5ba72ce81a02e4a23cae84f
Maxime Bury <maxime.bury@gmail.com>
2012-10-02 23:19:49 -0400

    Search sort of works, audio playback is functional

--- js/musicloader.js
+++ js/musicloader.js
@@ -9,14 +9,28 @@
     };
 
     loader.prototype.load = function(url) {
-        var audio = document.createElement('audio');
+        var source,
+            audio = document.createElement('audio'),
+            gain  = this.context.createGainNode(),
+            analyser = this.context.createAnalyser()
+        ;
+        
         audio.setAttribute('src', url + '?client_id=' + this.api_key);
         audio.load();
         
+        source = this.context.createMediaElementSource(audio);
+        source.connect(analyser);
+        analyser.connect(gain);
+        gain.connect(this.context.destination);
+        
+        
         this.sound_id += 1;
         this.sounds[this.sound_id] = {
+            "id": this.sound_id,
             "node": audio,
-            "source" : this.context.createMediaElementSource(audio)
+            "gainNode": gain,
+            "analyserNode": analyser,
+            "source" : source
         };
                 
         return this.sound_id;
@@ -24,7 +38,6 @@
     
     loader.prototype.play = function(sound_id) {
         var sound = this.sounds[sound_id];
-        sound.source.connect(this.context.destination);
         sound.source.mediaElement.play();
     };
     
@@ -36,6 +49,10 @@
     loader.prototype.remove = function(sound_id) {
         delete this.sounds[sound_id];
     };
+    
+    loader.prototype.get = function(sound_id) {
+        return this.sounds[sound_id];
+    }
     
     window.ML = new loader('c82759e173597444cb667171f18e7656');
 })();
